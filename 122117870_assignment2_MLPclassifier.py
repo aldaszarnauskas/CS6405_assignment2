50fdabac230210dfe7b2c9e5c3428154704c029e
aldaszarnauskas <109503215+aldaszarnauskas@users.noreply.github.com>
2023-04-09 08:23:11 +0100

    Update 122117870_assignment2_MLPclassifier.py

--- 122117870_assignment2_MLPclassifier.py
+++ 122117870_assignment2_MLPclassifier.py
@@ -1,1 +1,15 @@
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, chi2
 
+from sklearn.pipeline import Pipeline
+
+classifierMLP = Pipeline([
+    ('scaler', MinMaxScaler()),
+    ('pca', PCA(n_components=0.99, svd_solver='auto')),
+    ('scaler2', MinMaxScaler()),
+    ('selector', SelectKBest(k=25, score_func=chi2)),
+    ('clf', MLPClassifier(max_iter=1000, early_stopping=True, tol=0.000001, activation='tanh',\
+                         alpha=0.01, hidden_layer_sizes=(60), learning_rate='adaptive', solver='lbfgs',\
+                         ))
+])
